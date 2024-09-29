749c7ace6af0dbb0d333f38f5e9a8f8d95e8f64e
I'm Matheus <codelong@proton.me>
2024-09-29 11:22:29 -0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,42 +1,17 @@
 @echo off
-setlocal enabledelayedexpansion
 
-:: Função para verificar e instalar o Cloudflared
-:install_cloudflared
-where cloudflared >nul 2>nul
-if %errorlevel% equ 0 (
-    echo Cloudflared já está instalado.
-) else (
-    echo Instalando Cloudflared...
-    choco install cloudflared -y
+:: Instalação do Chocolatey
+where choco >nul 2>nul
+if %errorlevel% neq 0 (
+    echo Instalando Chocolatey...
+    powershell -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
 )
 
-:: Instalar o Java versão 17
-echo Instalando Java versão 17...
-choco install jdk17 -y
+:: Instalando Docker
+echo Instalando Docker...
+choco install docker-desktop -y
 
-:: Configurar e iniciar o Cloudflared
-echo Configurando o Cloudflared...
-start cmd /c "cloudflared tunnel --url localhost:25565"
-
-:: Aguardar a inicialização do Cloudflared
-echo Aguardando o Cloudflared iniciar...
-timeout /t 10 >nul
-
-:: Obter o IP do Cloudflared
-echo Verificando o IP do Cloudflared...
-set "cldflr_url="
-for /f "tokens=*" %%i in ('cloudflared tunnel list') do (
-    set "line=%%i"
-    if "!line!" neq "" (
-        set "cldflr_url=!line!"
-        echo URL do Cloudflared encontrado: !cldflr_url!
-    )
-)
-
-:: Iniciar o servidor Minecraft
-echo Iniciando o servidor Minecraft...
-java -Xmx1024M -Xms1024M -jar paper-1.21.1-110.jar nogui
-
-echo Servidor Minecraft iniciado com IP do Cloudflared: !cldflr_url!
-pause
+:: Levantar os containers com Docker Compose
+echo Iniciando o Docker Compose...
+docker-compose build
+docker-compose up
