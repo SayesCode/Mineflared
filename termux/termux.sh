10ee3c5e3763930482626d3a52ccacd15fb236e3
I'm Matheus <codelong@proton.me>
2024-10-05 00:11:59 -0300

    Update termux.sh

--- termux/termux.sh
+++ termux/termux.sh
@@ -1,5 +1,9 @@
 pkg update && pkg upgrade
+pkg install wget
 pkg install proot-distro
 proot-distro install ubuntu
 proot-distro login ubuntu
-bash ./build.sh
+# In the Ubutun run this commands:
+# git clone https://github.com/SayesCode/Mineflared.git
+# cd Mineflared
+# bash ./termux/build.sh
